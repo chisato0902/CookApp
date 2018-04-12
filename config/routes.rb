40afa632a958eb1a434a7c976338d6ebd935a694
阿部知里 <abechisato@Keico-no-MacBook-Air.local>
2018-04-12 17:31:35 +0900

    add CookPro

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts' => "posts#index"
   get 'home/about' => "home#about"
   get 'home/top' => "home#top"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'posts/new' => "posts#new"
+  get 'posts/:id' => "posts#show"
+  post 'posts/create' => "posts#create"
+  get 'posts' => "posts#index"
 end
